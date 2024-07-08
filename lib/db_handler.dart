27c38d1dc83072bbde05d13016c31a4fad966c61
tech-dhruv <72492061+tech-dhruv@users.noreply.github.com>
2024-07-08 09:06:49 +0530

    done

--- lib/db_handler.dart
+++ lib/db_handler.dart
@@ -9,36 +9,70 @@ class DBHelper {
 
   Future<Database?> get db async {
     if (_db != null) {
-      return _db;
+      return _db!;
     }
     _db = await initDatabase();
+    return _db!;
   }
 
   initDatabase() async {
     io.Directory documentDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentDirectory.path, 'notes.db');
+    String path = join(documentDirectory.path, 'noxodata.db');
     var db = await openDatabase(path, version: 1, onCreate: _onCreate);
     return db;
   }
 
   _onCreate(Database db, int version) async {
     await db.execute(
-      "CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, age INTEGER NOT NULL, description TEXT NOT NULL, email TEXT)",
+      '''CREATE TABLE notesdata (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, age INTEGER NOT NULL, description TEXT NOT NULL, email TEXT)''',
     );
   }
 
   Future<NotesModel> insert(NotesModel notesModel) async {
     var dbClient = await db;
     print("======");
-    await dbClient!.insert('notes', notesModel.toMap());
+    await dbClient!.insert('notesdata', notesModel.toMap());
     return notesModel;
   }
 
   Future<List<NotesModel>> getNotesList() async {
+    print("----------------------------------------------");
     var dbClient = await db;
     final List<Map<String, Object?>> queryResult =
-        await dbClient!.query('notes');
+        await dbClient!.query('notesdata');
 
     return queryResult.map((e) => NotesModel.fromMap(e)).toList();
   }
+
+  Future<int> deleteNotes(int id) async {
+    var dbClient = await db;
+    return await dbClient!.delete(
+      'notesdata',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> updateNotes(NotesModel notesModel) async {
+    var dbClient = await db;
+    return await dbClient!.update(
+      'notesdata',
+      notesModel.toMap(),
+      where: 'id = ?',
+      whereArgs: [notesModel.id],
+    );
+  }
+
+  Future deleteTableContent() async {
+    var dbClient = await db;
+    return await dbClient!.delete(
+      'notesdata',
+    );
+  }
+
+  Future close() async {
+    var dbClient = await db;
+    dbClient!.close();
+  }
+
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -22,7 +22,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   loadData() async {
-    notesList =  dbHelper!.getNotesList();
+    setState(() {
+      notesList = dbHelper!.getNotesList();
+    });
   }
 
   @override
@@ -38,35 +40,83 @@ class _HomeScreenState extends State<HomeScreen> {
             child: FutureBuilder(
               future: notesList,
               builder: (context, AsyncSnapshot<List<NotesModel>> snapshot) {
-               return ListView.builder(
-                 shrinkWrap: true,
-                  itemCount:  snapshot.data?.length,
-                  itemBuilder: (context, index) {
-                    return snapshot.data?.length == 0 ? Container() : Card(
-                      child: ListTile(
-                        contentPadding: const EdgeInsets.all(8),
-                        title: Text(snapshot.data![index].title.toString()),
-                        subtitle: Text(snapshot.data![index].email.toString()),
-                        trailing: Text(snapshot.data![index].age.toString()),
-                      ),
-                    );
-                  },
-                );
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasError) {
+                  return const Center(
+                    child: Text('Error loading data'),
+                  );
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(
+                    child: Text('No data found'),
+                  );
+                } else {
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          dbHelper!.updateNotes(NotesModel(
+                            id: snapshot.data![index].id!,
+                            title: 'Second Flutter note',
+                            age: 11,
+                            description: 'hello Now i am busy',
+                            email: 'dhyan.de.54@gmail.com',
+                          ));
+                          setState(() {
+                            notesList = dbHelper!.getNotesList();
+                          });
+                        },
+                        child: Dismissible(
+
+                          direction: DismissDirection.endToStart,
+
+                          background: const Card(
+                            color: Colors.redAccent,
+                            child: Icon(Icons.delete_forever),
+                          ),
+                          onDismissed: (direction) {
+                            setState(() {
+                              dbHelper!.deleteNotes(snapshot.data![index].id!);
+                              notesList = dbHelper!.getNotesList();
+                              snapshot.data!.remove(snapshot.data![index]);
+                            });
+                          },
+                          key: ValueKey(snapshot.data![index].id!),
+                          child: Card(
+                            child: ListTile(
+                              contentPadding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                              title: Text(snapshot.data![index].title),
+                              subtitle: Text(snapshot.data![index].email),
+                              trailing:
+                                  Text(snapshot.data![index].age.toString()),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
               },
             ),
-          )
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           dbHelper!
               .insert(NotesModel(
-                  title: 'First Note',
-                  age: 22,
-                  description: 'this is my first app',
-                  email: 'dsenjaliya54@gmail.com'))
+            title: 'First Note',
+            age: 22,
+            description: 'This is my first app',
+            email: 'dsenjaliya54@gmail.com',
+          ))
               .then((value) {
-            print('data added');
+            print('Data added');
+            loadData();
           }).onError((error, stackTrace) {
             print(error.toString());
           });
